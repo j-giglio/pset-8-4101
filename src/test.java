4b3aade65e63247e4c12356841ffdb74bb0b18c5
j-giglio <55398564+j-giglio@users.noreply.github.com>
2021-01-24 19:23:56 -0500

    Delete test.java

--- src/test.java
+++
@@ -1,7 +0,0 @@
-public class test {
-    public static void main(String[] args) {
-        ProblemSet8 test = new ProblemSet8();
-        int[] b = {6,5,9,5,5,4};
-        System.out.println(test.halfway(b));
-    }
-}
